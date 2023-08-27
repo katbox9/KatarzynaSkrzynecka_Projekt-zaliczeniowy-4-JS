20df96da7f160dfb669d31d8ce0ca46e8ec23d20
Katarzyna Szewczyk-Skrzynecka <k.szewczyk.skrzynecka@gmail.com>
2023-08-27 20:04:39 +0200

    create recuired consts for creating incomes tables for revenues and expenses

---
+++ index.js
@@ -0,0 +1,7 @@
+const incomeRevenuesForm = document.getElementById("revenues-form");
+const incomeRevenuesList = document.getElementById("revenues-list");
+const incomesRevenues = [];
+
+const incomeExpensesForm = document.getElementById("expenses-form");
+const incomeExpensesList = document.getElementById("expenses-list");
+const incomesExpenses = [];
