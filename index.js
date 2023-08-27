d038314e4a0c2aa17193aa21635726d05d9387e5
Katarzyna Szewczyk-Skrzynecka <k.szewczyk.skrzynecka@gmail.com>
2023-08-27 20:41:39 +0200

    create income revenues list function

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const incomeRevenuesForm = document.getElementById("revenues-form");
 const incomeRevenuesList = document.getElementById("revenues-list");
 const incomesRevenues = [];
 
-const incomeExpensesForm = document.getElementById("expenses-form");
-const incomeExpensesList = document.getElementById("expenses-list");
-const incomesExpenses = [];
+// const incomeExpensesForm = document.getElementById("expenses-form");
+// const incomeExpensesList = document.getElementById("expenses-list");
+// const incomesExpenses = [];
+
+incomeRevenuesForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newName = event.target.revenueName.value;
+  const newAmount = event.target.revenueAmount.value;
+
+  incomesRevenues.push({
+    name: newName,
+    amount: newAmount,
+    id: Math.random(),
+  });
+
+  renderIncomeRevenuesList();
+});
