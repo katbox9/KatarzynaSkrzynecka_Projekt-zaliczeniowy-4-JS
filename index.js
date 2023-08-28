df26a924ff96f67a4613510ee4e951dcb21ae923
Katarzyna Szewczyk-Skrzynecka <k.szewczyk.skrzynecka@gmail.com>
2023-08-28 15:42:07 +0200

    create render income revenues list

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ const incomesRevenues = [];
 // const incomeExpensesList = document.getElementById("expenses-list");
 // const incomesExpenses = [];
 
+function renderIncomeRevenuesList() {
+  incomeRevenuesList.innerHTML = "";
+  incomesRevenues.forEach((income) => {
+    const item = document.createElement("li");
+    item.textContent = `${income.title}: ${income.amount} PLN`;
+    incomeRevenuesList.appendChild(item);
+  });
+}
+
 incomeRevenuesForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const newName = event.target.revenueName.value;
+  const newTitle = event.target.revenueTitle.value;
   const newAmount = event.target.revenueAmount.value;
 
   incomesRevenues.push({
-    name: newName,
+    title: newTitle,
     amount: newAmount,
     id: Math.random(),
   });
