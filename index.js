a857e53e3f904a10ba21a02c8ba11ce82ca657cf
Katarzyna Szewczyk-Skrzynecka <k.szewczyk.skrzynecka@gmail.com>
2023-08-29 20:21:00 +0200

    create edit and delete buttons, create total revenues function

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ function renderIncomeRevenuesList() {
   incomesRevenues.forEach((income) => {
     const item = document.createElement("li");
     item.textContent = `${income.title}: ${income.amount} PLN`;
+
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edytuj";
+    // editBtn.setAttribute("type", "button");
+    editBtn.classList.add("btn", "btn-primary");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Usuń";
+    // editBtn.setAttribute("type", "reset");
+    deleteBtn.classList.add("btn", "btn-danger");
     incomeRevenuesList.appendChild(item);
+    item.appendChild(editBtn);
+    item.appendChild(deleteBtn);
   });
 }
 
@@ -23,9 +34,18 @@ incomeRevenuesForm.addEventListener("submit", (event) => {
 
   incomesRevenues.push({
     title: newTitle,
-    amount: newAmount,
+    amount: Number(newAmount),
     id: Math.random(),
   });
 
+  document.getElementById("revenues-form").reset();
+  totalRevenues();
+
   renderIncomeRevenuesList();
 });
+
+function totalRevenues() {
+  const total = incomesRevenues.reduce((prev, curr) => prev + curr.amount, 0);
+  const revenueTotal = document.getElementById("revenue-sum");
+  revenueTotal.textContent = `Suma przychodów = ${total} PLN`;
+}
